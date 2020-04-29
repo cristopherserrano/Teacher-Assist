0c07cbeec13c5f00441ab493a5075d5c6ac991d3
Cristopher Serrano <criss1913@gmail.com>
2020-04-29 18:14:07 -0400

    set up environment for application in docker

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,9 @@
+import uuid
+import os
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
+from django.conf import settings
 
 
 class UserManager(BaseUserManager):
@@ -22,11 +25,81 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Classroom(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    time = models.DateTimeField()
+    students = models.ManyToManyField('student')
+    grades = models.ManyToManyField('grade')
+
+    def __str__(self):
+        return self.name
+
+
+class Grade(models.Model):
+    letter_grade = models.CharField(max_length=5)
+    value = models.IntegerField()
+    value_received = models.IntegerField()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    percentage_of_final_grade = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    YEAR_IN_SCHOOL_CHOICES = [
+        (0, 'K'),
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+        (11, '11'),
+        (12, '12'),
+    ]
+    GENDER = [
+        (0, 'Male'),
+        (1, 'Female'),
+    ]
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    grade_level = models.IntegerField(
+        choices=YEAR_IN_SCHOOL_CHOICES,
+    )
+    age = models.IntegerField()
+    gender = models.IntegerField(
+        choices=GENDER,
+    )
+    disability = models.BooleanField(default=False)
+    grade = models.ManyToManyField('grade')
+
+    def __str__(self):
+        return self.name
